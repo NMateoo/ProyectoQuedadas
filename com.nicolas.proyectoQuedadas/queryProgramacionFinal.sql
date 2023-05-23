858354ed6efc61fa02ed6c7a26011b84269fa467
Nicolás Mateo <55945912+NMateoo@users.noreply.github.com>
2023-05-23 22:07:48 +0200

    Update queryProgramacionFinal.sql

--- com.nicolas.proyectoQuedadas/queryProgramacionFinal.sql
+++ com.nicolas.proyectoQuedadas/queryProgramacionFinal.sql
@@ -5,18 +5,20 @@ create database quedadas;
 use quedadas;
 
 create table persona(
-	email varchar(50) primary key,
+	email varchar(50),
     nombre varchar(100),
     telefono int(10),
     genero character(1),
     contraseña varchar(50),
-    mascota varchar(40)
+    mascota varchar(40),
+    primary key(email)
 );
 
 create table mascota(
 	nombre varchar(50),
     animal varchar(20),
-    dueño varchar(50)
+    dueño varchar(50),
+    primary key(nombre,dueño)
 );
 
 create table perro(
@@ -34,6 +36,7 @@ create table gato(
 );
 
 create table actividadEquipos(
+	nombre varchar(200),
 	lugar varchar(100),
     privada boolean,
     precio float,
@@ -42,17 +45,16 @@ create table actividadEquipos(
     minParticipantes int(3),
     maxParticipantes int(3),
     categoria varchar(100),
-    nombre varchar(200),
     constraint claveActividad primary key(nombre,lugar,momento)
 );
 
 create table equipo(
+	nombre varchar(100),
 	tamañoMaximo int(3),
     nombreActividad varchar(200),
     lugarActividad varchar(100),
     momentoActividad timestamp,
-    nombre varchar(100),
     primary key(nombre,nombreActividad,lugarActividad,momentoActividad),
-    CONSTRAINT actividadEquipos FOREIGN KEY (nombreActividad, lugarActividad, momentoActividad) 
+    constraint actividadEquipos FOREIGN KEY (nombreActividad, lugarActividad, momentoActividad) 
         REFERENCES actividadEquipos(nombre, lugar, momento)
 );
